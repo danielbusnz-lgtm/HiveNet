e220dc70d86f1a17872839c5365189ed155424fe
danielbusnz-lgtm <danielbusnz@gmail.com>
2026-04-19 13:59:31 -0400

    Added Middleware For Backend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.users import router as users_router
 from app.routes.posts import router as posts_router
 from app.routes.follows import router as follows_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router)
 app.include_router(posts_router)
